ca91abc0223d42e3d89da30a1fa4bb89de0b1d93
Andrei Dochkin <andreydochkin@yandex.ru>
2023-07-18 15:28:33 +0200

    feat: add Portfolio

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,26 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Route, Navigate, Routes, useNavigate } from 'react-router-dom';
 
 import Header from '../Header/Header';
+import Promo from "../Promo/Promo";
+import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
+import Profile from "../Profile/Profile";
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
+import Portfolio from "../Portfolio/Portfolio";
 
 function App() {
   return (
-      <div className="app">
-        <Header />
-        {/* <Main />
-        <Footer /> */}
-      </div>
+    <div className="app">
+
+      <Header />
+      <Promo />
+      <AboutProject />
+      <Techs />
+      <Portfolio />
+
+    </div>
   );
 }
 
---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import photoPath from '../../images/document_photo_2.jpg';
+import arrowPath from '../../images/arrow.svg';
+
+function Portfolio() {
+    return (
+        <section className='portfolio'>
+            <h2 className='portfolio__title'>Студент</h2>
+            <div className='portfolio__line'></div>
+
+            <div className='portfolio__article-container'>
+                <article className='portfolio__article'>
+                    <h3 className='portfolio__article-title'>Андрей</h3>
+                    <p className='portfolio__article-subtitle'>Фронтенд-разработчик, 35 лет</p>
+                    <p className='portfolio__article-text'>
+                        Я родился в Санкт-Петербурге и окончил электротехнический университет "ЛЭТИ". Мой карьерный путь начался с разработки встраиваемых систем на C/C++. Недавно я закончил курс по веб-разработке от Practicum от Yandex, где углубил свои знания и навыки в области веб-разработки. Теперь я уверен в своей способности успешно применять свой опыт и умения для создания современных и инновационных веб-приложений, используя языки JavaScript, HTML, CSS, а также фреймворки React и Node.js.
+                    </p>
+                    <a className='portfolio__article-link' href='https://github.com/AndreyDochkin'>GitHub</a>
+                </article>
+                <img className='portfolio__article-img' src={photoPath} alt='Portfolio author' />
+            </div>
+
+            <h3 className='portfolio__list-title'>Портфолио</h3>
+            <ul className='portfolio__list'>
+
+                <li className='portfolio__list-item'>
+                    <a className='portfolio__list-link' href='https://github.com/AndreyDochkin/how-to-learn'>Статичный сайт</a>
+                    <img className='portfolio__list-img' src={arrowPath} alt="Arrow icon" />
+                </li>
+
+                <div className='portfolio__list-line'></div>
+
+                <li className='portfolio__list-item'>
+                    <a className='portfolio__list-link' href='https://github.com/AndreyDochkin/russian-travel'>Адаптивный сайт</a>
+                    <img className='portfolio__list-img' src={arrowPath} alt="Arrow icon" />
+                </li>
+
+                <div className='portfolio__list-line'></div>
+
+                <li className='portfolio__list-item'>
+                    <a className='portfolio__list-link' href='https://github.com/AndreyDochkin/react-mesto-api-full-gha'>Одностраничное приложение</a>
+                    <img className='portfolio__list-img' src={arrowPath} alt="Arrow icon" />
+                </li>
+            </ul>
+
+        </section>
+    );
+}
+
+export default Portfolio;
